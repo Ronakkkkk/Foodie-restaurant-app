72ed4589cf75c4eb0a69ef335cf46c26014a4c22
Ronak <ronak01.raj@gmail.com>
2023-06-19 17:33:38 +0530

    fav_page

---
+++ lib/screens/Favourite_screen/fav_ingi.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:foodie/constants/colors.dart';
+import 'package:foodie/widgets/cloud_image_loader.dart';
+import 'package:foodie/widgets/ingredients.dart';
+
+class Favingi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List ingis = [
+      'assets/images/steak.png',
+      'assets/images/cheese.png',
+      'assets/images/vegetable.png'
+    ];
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 50,
+            child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return _ingicontext(index, ingis);
+                },
+                separatorBuilder: ((context, index) => const SizedBox(
+                      width: 10,
+                    )),
+                itemCount: ingis.length),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _ingicontext(int index, List ingis) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 45,
+          width: 45,
+          padding: const EdgeInsets.all(8),
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: kBackground,
+          ),
+          // child: Text('${list[index]}'),
+          child: Image(
+            image: AssetImage(ingis[index]),
+            fit: BoxFit.contain,
+          ),
+        ),
+      ],
+    );
+  }
+}
