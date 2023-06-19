cc0ad99e9ccfa4f44ca2a7c1729fe78e45a4a031
Ronak <ronak01.raj@gmail.com>
2023-06-20 02:33:52 +0530

    fav_screen

--- lib/screens/Favourite_screen/widgets/Firebase.dart
+++ lib/screens/Favourite_screen/widgets/Firebase.dart
@@ -1,15 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FavFirebaseService {
   final CollectionReference usersCollection =
       FirebaseFirestore.instance.collection('users');
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<void> addUser(Map<String, dynamic> userData) async {
+  Future<String?> getCurrentUserId() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      return user.uid;
+    }
+    return null;
+  }
+
+  Future<void> addFav(Map<String, dynamic> userData, String? uid) async {
     try {
-      await usersCollection.doc('d_id').set(userData);
-      await usersCollection.doc('d_id').collection('favourites').doc().set({
-        'favoriteItem': 'exampleItem',
-      });
+      await usersCollection
+          .doc(uid)
+          .collection('favourites')
+          .doc()
+          .set({'name': userData['name'], 'cuisine': userData['cuisine']});
       print('User data added successfully.');
     } catch (e) {
       print('Error adding user data: $e');
