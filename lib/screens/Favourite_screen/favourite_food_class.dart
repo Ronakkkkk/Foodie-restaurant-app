b3212775cca066725c4b8e44e20936fa95ea5050
Ronak <ronak01.raj@gmail.com>
2023-06-19 17:33:42 +0530

    fav_page

--- lib/screens/Favourite_screen/favourite_food_class.dart
+++ lib/screens/Favourite_screen/favourite_food_class.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:foodie/screens/Favourite_screen/fav_ingi.dart';
 
 import '../../constants/colors.dart';
 import '../../constants/texts.dart';
@@ -11,7 +14,7 @@ class FoodTile extends StatelessWidget {
       child: Stack(
         children: [
           Container(
-            height: 190,
+            height: 200,
             width: double.maxFinite,
             margin:
                 const EdgeInsets.only(top: 0, right: 25, left: 25, bottom: 15),
@@ -23,13 +26,48 @@ class FoodTile extends StatelessWidget {
               //     fit: BoxFit.cover),
             ),
             child: Container(
-              padding: EdgeInsets.all(20),
+              padding:
+                  EdgeInsets.only(top: 30, right: 20, left: 20, bottom: 20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     'Chicken MoMo',
-                    style: kBigText.copyWith(color: kIconColor, fontSize: 20),
+                    style:
+                        kBigText.copyWith(color: kPrimaryColor, fontSize: 25),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'Nepali Delights',
+                    style: kSmallText.copyWith(
+                        fontSize: 20, color: const Color(0xff747478)),
+                  ),
+                  const SizedBox(height: 8),
+                  Favingi(),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Row(children: [
+                      Text(
+                        "Rs.",
+                        style: kSmallText.copyWith(
+                            fontSize: 15,
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontFeatures: [const FontFeature.superscripts()]),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        '200',
+                        style: kSmallText.copyWith(
+                            fontSize: 20,
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ]),
                   ),
                 ],
               ),
@@ -39,8 +77,8 @@ class FoodTile extends StatelessWidget {
             right: 5,
             bottom: 0,
             child: Container(
-              height: 140,
-              width: 140,
+              height: 130,
+              width: 130,
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 color: Colors.white,
